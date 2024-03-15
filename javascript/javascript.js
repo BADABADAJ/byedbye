b2923fb00490f383fd88d7abe606a02cd5543c1b
BADABADAJ <160164433+BADABADAJ@users.noreply.github.com>
2024-03-15 19:55:27 -0400

    git push

---
+++ javascript/javascript.js
@@ -0,0 +1,27 @@
+window.onload = function(){ 
+
+const track = document.getElementById("image-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt =  e.clientX;
+}
+
+window.onmousemove = e => {
+if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth/2;
+
+    const percentage = (mouseDelta/maxDelta)*-100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage)+ percentage;
+
+    track.dataset.percentage=nextPercentage;
+
+    track.style.transform = `translate(${percentage}%, -50%)`;
+}
+
+window.onmouseup = function () {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+}
